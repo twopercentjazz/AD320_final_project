bbb2e3b5e00647810dc76e07d509338bb95e66c7
jinjou <jinjou2@yahoo.com>
2023-12-06 15:33:51 -0800

    all rooms query

--- queries.sql
+++ queries.sql
@@ -17,3 +17,30 @@ dates	1704067199-2147483648
 
 --	Turn on foreign key enforcement
 PRAGMA foreign_keys=1;
+
+
+--	all rooms
+SELECT r."id",r."number",r."max",t."type",b."label",r."count",r."rate",r."picture"
+FROM "rooms" r,"types" t,"beds" b
+WHERE t."id"=r."type" AND b."id"=r."bed";
+
+
+--	filter rooms
+
+
+--	create account
+
+
+--	login
+
+
+--	logout
+
+
+--	view trans
+
+
+--	all trans per user
+
+
+--	make res
